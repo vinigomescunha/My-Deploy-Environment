9ef30058a733a0737ad75f10f860a2c5cf59ada5
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 13:09:33 -0200

    On pre-push update docker container from git

--- my-scripts/create-git-deploy-request.sh
+++ my-scripts/create-git-deploy-request.sh
@@ -3,4 +3,5 @@
 dockerimagetag="vgcmdedockerimage:1.0"
 # entendo que so tem uma instancia do docker com essa imagem rodando...
 firstdockerinstance=$(sudo docker ps -a | grep -P "${dockerimagetag}" | awk 'NR==1{print $1}')
-docker exec $firstdockerinstance $(realpath "./pull-clone-repository.sh")
+sudo docker exec -it $firstdockerinstance /bin/bash -c "$(realpath './my-scripts/pull-clone-repository.sh') "
+
