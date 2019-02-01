b6ebc11b324ebf77d559d2045f28a1c874621f9c
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 13:16:41 -0200

    fix port 9000

--- my-app/index.js
+++ my-app/index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen( process.env.PORT || 3000 , function () {
+app.listen( process.env.PORT || 9000 , function () {
   console.log(`Listening on port ${(process.env.PORT || 9000)}!`);
 });
