4a5d0d7fc462fa00e85c80e49a567ba59b3345e3
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:14:00 -0200

    Refactoring create-docker-image

--- my-scripts/create-docker-image.sh
+++ my-scripts/create-docker-image.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-dockerimagetag="vgcmdedockerimage:1.0"
+# title           : create-docker-image.sh
+# description     : list the docker images and find if exist specific image, else create 
+# author          : vinigomescunha
+# date            : 2019-02-01
+# bash_version    : 4.2
+#==============================================================================
+source "config.sh"
 while IFS= read -r line; do
-  if [ "$line" == "$dockerimagetag" ]; then
+  if [ "$line" == "$DOCKER_IMG_TAG" ]; then
     echo "Image docker: $line already exists!"
     sleep 2
     exit 0;
   fi
 done < <( sudo docker images | awk '{if(NR!=1) print $1":"$2}' )
-sudo docker build -t ${dockerimagetag} https://github.com/vinigomescunha/My-Deploy-Environment.git
+sudo docker build -t ${DOCKER_IMG_TAG} ${GIT_REPOSITORY}
