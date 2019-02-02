998f0cecaed961c335fbc1128b649b02a17684fa
vinigomescunha <vinigomescunha@gmail.com>
2019-02-02 04:02:07 -0200

    remove wololo

--- my-app/index.js
+++ my-app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Wololo');
+  res.send('Hello World!');
 });
 
 app.listen( process.env.PORT || 9000 , function () {
