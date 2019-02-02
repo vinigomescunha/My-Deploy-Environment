86719001a51c75a08fddf719c19d9c116b0e2d79
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:32:52 -0200

    update hooks header

--- my-scripts/create-hooks.sh
+++ my-scripts/create-hooks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# title           : create-hooks.sh
+# description     : Insert hooks into git repository to custom activities during commit, push, ...  
+# author          : vinigomescunha
+# date            : 2019-02-01
+# bash_version    : 4.2
+#==============================================================================
 for hook in ./my-git-hooks/* ; do
   ln -s -f $(realpath $hook) ./.git/hooks/$(basename $hook)
 done
