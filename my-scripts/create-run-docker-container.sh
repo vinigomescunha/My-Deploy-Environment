4d18b90ead89a98fbc6b6f38e4257393a7f6b7d6
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:41:00 -0200

    update scripts that run container

--- my-scripts/create-run-docker-container.sh
+++ my-scripts/create-run-docker-container.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-# get the first container and run
-# kill port
-dockerimagetag="vgcmdedockerimage:1.0"
-firstdockerinstance=$(sudo docker ps -a | grep -P "${dockerimagetag}" | awk 'NR==1{print $1}')
-
+# title           : create-run-docker-container.sh
+# description     : Insert hooks into git repository to custom activities during commit, push, ...  
+# author          : vinigomescunha
+# date            : 2019-02-01
+# bash_version    : 4.2
+#==============================================================================
+source "config.sh"
+firstdockerinstance=$(sudo docker ps -a | grep -P "${DOCKER_IMG_TAG}" | awk 'NR==1{print $1}')
 function kill_process_port {
   if [[ $(eval "lsof -i:$1") ]]; then
-    echo " Killing port: $1  ";
+    echo " Killing port: $1! ";
     eval "fuser -k $1/tcp" 2>&1
   else
-    echo "Port $1 is iddle"
+    echo "Port $1 is iddle!"
   fi
 }
 function stop_all_instances_running {
-  for container in  $(sudo docker ps | grep -P "${dockerimagetag}" | awk '{print $1}'  ); do
-    echo "$container"
-    sudo docker stop $container
+  for container in  $(sudo docker ps | grep -P "${DOCKER_IMG_TAG}" | awk '{print $1}'  ); do
+    echo "$container found..."
+    eval "sudo docker stop $container"
   done
 }
-kill_process_port "9000"
+kill_process_port "${PORT_SERVER}"
 stop_all_instances_running
 if [ "" == "${firstdockerinstance}" ]; then
-    sudo docker run -p 9000:9000 -d -i ${dockerimagetag}
+    eval "sudo docker run -p ${PORT_SERVER}:${PORT_SERVER} -d -i ${DOCKER_IMG_TAG}"
   else
-    sudo docker start ${firstdockerinstance}
+    eval "sudo docker start ${firstdockerinstance}"
 fi
