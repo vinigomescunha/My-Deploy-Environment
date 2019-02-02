bee1df73036b6e763020aaea3661794cfd772900
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:34:09 -0200

    during push update docker repository

---
+++ my-scripts/create-git-deploy-request-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# title           : create-git-deploy-request-docker.sh
+# description     : Script that will be executed in the docker container 
+# author          : vinigomescunha
+# date            : 2019-02-01
+# bash_version    : 4.2
+#==============================================================================
+function allocate_script_remote {
+  echo "Script is executing in the docker container"
+  cd /my-app
+  sleep 2
+  if [ "$(git rev-parse --is-inside-work-tree)" == "true" ]; then
+    echo "Update git repository"
+    eval "git pull ${GIT_PROTOCOL}${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@${GIT_REPOSITORY}" 2>&1
+  else
+    echo "Cloning repository"
+    eval "git clone ${GIT_PROTOCOL}${GIT_REPOSITORY} . " 2>&1
+    npm i
+    nodemon ./index.js localhost 9000
+  fi
+}
--- my-scripts/create-git-deploy-request.sh
+++ my-scripts/create-git-deploy-request.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# or pull or clone repository
-dockerimagetag="vgcmdedockerimage:1.0"
-# entendo que so tem uma instancia do docker com essa imagem rodando...
-firstdockerinstance=$(sudo docker ps -a | grep -P "${dockerimagetag}" | awk 'NR==1{print $1}')
-sudo docker exec -it $firstdockerinstance /bin/bash -c "$(realpath './my-scripts/pull-clone-repository.sh') "
-
+# title           : create-gitdeploy-request.sh
+# description     : Send script to update repo in in the docker instance 
+# author          : vinigomescunha
+# date            : 2019-02-01
+# bash_version    : 4.2
+#==============================================================================
+source "config.sh"
+source $(realpath './my-scripts/create-git-deploy-request-docker.sh')
+CONFIG_VARS="$(cat 'config.sh')  | sed 's/\"/\x27'"
+firstdockerinstance="$(sudo docker ps -a | grep -P "${DOCKER_IMG_TAG}" | awk 'NR==1{print $1}')"
+if [ ! -z "$firstdockerinstance" ]; then
+  sudo docker exec -it $firstdockerinstance /bin/bash -c "
+  $CONFIG_VARS
+  $(typeset -f allocate_script_remote)
+  allocate_script_remote"
+else
+  echo "No container running..."
+fi
