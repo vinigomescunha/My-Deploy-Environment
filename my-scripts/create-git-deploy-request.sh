3b888975d6d65e37629bdc5150e82055a47dc691
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:45:31 -0200

    fix source

--- my-scripts/create-git-deploy-request.sh
+++ my-scripts/create-git-deploy-request.sh
@@ -5,7 +5,7 @@
 # date            : 2019-02-01
 # bash_version    : 4.2
 #==============================================================================
-source "config.sh"
+source "./config.sh"
 source $(realpath './my-scripts/create-git-deploy-request-docker.sh')
 CONFIG_VARS="$(cat 'config.sh')  | sed 's/\"/\x27'"
 firstdockerinstance="$(sudo docker ps -a | grep -P "${DOCKER_IMG_TAG}" | awk 'NR==1{print $1}')"
