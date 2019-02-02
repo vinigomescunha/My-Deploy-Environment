55bd144cd20c143978cc09c3037c094144a221fd
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:04:45 -0200

    update config

---
+++ config.sh
@@ -0,0 +1,7 @@
+echo "variables..."
+export GIT_REPOSITORY="github.com/vinigomescunha/My-Deploy-Environment.git"
+export DOCKER_IMG_TAG="vgcmdedockerimage:1.0"
+export GIT_USERNAME="vinigomescunha"
+# below https://github.com/settings/tokens :  "admin:repo_hook" and "repo"
+export GIT_ACCESS_TOKEN=""
+export GIT_PROTOCOL="https://"
