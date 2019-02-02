7777250c49986170916442e9fc335e8aa21f1e4d
vinigomescunha <vinigomescunha@gmail.com>
2019-02-01 23:10:25 -0200

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source './config.sh'
 source "./my-scripts/create-hooks.sh"
 action=""
 help() { echo "Arguments: $0 [-i : Create Image ] [-c : Create Container ]" 1>&2; exit 1; }
 create_docker_image() { source "./my-scripts/create-docker-image.sh" 1>&2; exit 1;  }
-create_run_docker_container() { source "/my-scripts/create-run-docker-container.sh" 1>&2; exit 1;  }
+create_run_docker_container() { source "./my-scripts/create-run-docker-container.sh" 1>&2; exit 1;  }
 while getopts "ic" o; do
   action="${o}"
   case "${o}" in
